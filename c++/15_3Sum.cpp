f92c6bc1cb791ea47e4809da8dcb02fef6f4daa0
Pelhans <me@pelhans.com>
2018-03-29 09:14:01 +0800

    add 1 and 15 with c++

---
+++ c++/15_3Sum.cpp
@@ -0,0 +1,41 @@
+/*************************************************************************
+	> File Name: c++/15_3Sum.cpp
+	> Author: Pelhans 
+	> Mail: me@pelhans.com
+	> Created Time: 2018年03月29日 星期四 09时12分56秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        if (nums.size() < 3) return result;
+        sort(nums.begin(), nums.end());
+        const int target = 0;
+        
+        auto last = nums.end();
+        for (auto i = nums.begin(); i < last - 2; i++){
+            auto j = i + 1;
+            if ( i > nums.begin() && *i == *(i-1) ) continue;
+            auto k = last - 1;
+            while (j < k){
+                if (*i + *j + *k < target){
+                    ++j;
+                    while (*j == *(j-1) && j < k)  ++j;
+                }else if ( *i + *j + *k > target){
+                    --k;
+                    while ( *k == *(k+1) && j<k) --k;
+                }else{
+                    result.push_back({*i, *j, *k});
+                    ++j;
+                    --k;
+                    while ( *j == *(j-1) && *k == *(k+1) && j<k ) ++j;
+                }
+            }
+        }
+        return result;
+    }
+};
