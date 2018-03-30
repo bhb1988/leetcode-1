fe0e1c30ae5bf014236f290f6e451519e52c082d
Pelhans <me@pelhans.com>
2018-03-30 10:03:48 +0800

    add 60 with c++

---
+++ c++/60_Permutation_Sequence.cpp
@@ -0,0 +1,46 @@
+/*************************************************************************
+	> File Name: c++/60_Permutation_Sequence.cpp
+	> Author: Pelhans 
+	> Mail: me@pelhans.com
+	> Created Time: 2018年03月30日 星期五 10时03分17秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s(n, '0');
+        string result;
+        for (int i = 0; i < n; ++i)
+            s[i] += i+1;
+        return kth_permutation(s, k);
+    }
+private:
+    int factorial(int n){
+        int result = 1;
+        for (int i = 1; i <= n; ++i)
+            result *= i;
+        return result;
+    }
+    
+    template<typename Sequence>
+    Sequence kth_permutation(const Sequence &seq, int k){
+        const int n = seq.size();
+        Sequence S(seq); //??
+        Sequence result;
+        
+        int base = factorial(n-1);
+        --k;
+        
+        for (int i = n-1; i > 0; k %= base, base /= i, --i){
+            auto a = next(S.begin(), k / base);
+            result.push_back(*a);
+            S.erase(a);
+        }
+        
+        result.push_back(S[0]);
+        return result;
+    }
+};
