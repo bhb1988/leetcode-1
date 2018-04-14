50d0d4b7767e24aa539517b4c60a61e169465121
Pelhans <me@pelhans.com>
2018-04-14 15:39:18 +0800

    add 86 with cpp

---
+++ c++/86_Partition_List.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: c++/86_Partition_List.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年04月14日 星期六 15时38分32秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode left_dummy(-1);
+        ListNode right_dummy(-1);
+        
+        auto left_cur = &left_dummy;
+        auto right_cur = &right_dummy;
+        
+        for (ListNode *cur = head; cur; cur = cur->next){
+            if (cur->val < x){
+                left_cur->next = cur;
+                left_cur = cur;
+            }
+            else{
+                right_cur->next = cur;
+                right_cur = cur;
+            }
+        }
+        left_cur->next = right_dummy.next;
+        right_cur->next = nullptr;
+        return left_dummy.next;
+    }
+};
