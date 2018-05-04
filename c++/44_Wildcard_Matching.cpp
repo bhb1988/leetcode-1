c5549b1e4124c8449f0d34ca708aa1c5e3509beb
Peng Zhang <me@pelhans.com>
2018-05-04 09:23:51 +0800

    add 44 with c++

---
+++ c++/44_Wildcard_Matching.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return isMatch(s.c_str(), p.c_str());
+    }
+private:
+    bool isMatch(const char *s, const char *p){
+        bool star = false;
+        const char *str, *ptr;
+        for (str = s, ptr = p; *str != '\0'; str++, ptr++){
+            switch(*ptr){
+                case '?':
+                    break;
+                case '*':
+                    star = true;
+                    s = str, p = ptr;
+                    while (*p == '*') p++;
+                    if (*p == '\0') return true;
+                    str = s - 1;
+                    ptr = p - 1;
+                    break;
+                default:
+                    if (*str != *ptr){
+                    if (!star) return false;
+                    s++;
+                    str = s - 1;
+                    ptr = p - 1;
+                }
+            }
+        }
+        while (*ptr == '*') ptr++;
+        return (*ptr == '\0');
+    }
+};
