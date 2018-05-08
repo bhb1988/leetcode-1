0fc4ff2fa8cb54ebfdf377873903601cc1f7712c
Peng Zhang <me@pelhans.com>
2018-05-08 22:09:42 +0800

    add 144 with c++

---
+++ c++/144_Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        TreeNode *cur = root, *prev = nullptr;
+        
+        while (cur != nullptr){
+            if (cur->left == nullptr){
+                result.push_back(cur->val);
+                prev = cur;
+                cur = cur->right;
+            }else{
+                TreeNode *node = cur->left;
+                while (node->right != nullptr && node->right != cur)
+                    node = node->right;
+                
+                if (node->right == nullptr){
+                    result.push_back(cur->val);
+                    node->right = cur;
+                    prev = cur;
+                    cur = cur->left;
+                }else{
+                    node->right = nullptr;
+                    cur = cur->right;
+                }
+            }
+        }
+        return result;
+    }
+};
